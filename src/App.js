842b1eefcb69a53938a782609ab9fe7ce4e082bd
manikantaDev2510 <balu25102002@gmail.com>
2024-12-28 20:44:49 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import About from './pages/About'
+import Services from './pages/Services'
+import Packages from './pages/Packages'
+import Destination from './pages/Destination'
+import Booking from './pages/Booking'
+import Team from './pages/Team'
+import Error from './pages/Error'
+import Contact from './pages/Contact'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Header />
+        <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/services' element={<Services/>}/>
+        <Route path='/packages' element={<Packages/>}/>
+        <Route path='/destination' element={<Destination/>}/>
+        <Route path='/booking' element={<Booking/>}/>
+        <Route path='/team' element={<Team/>}/>
+        <Route path='/error' element={<Error/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        </Routes>
+        <Footer />
     </div>
-  );
+  )
 }
-
-export default App;
